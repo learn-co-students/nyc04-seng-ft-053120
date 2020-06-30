80db240d52d0ad9e34667c5676c4975d85aa63ca
Michelle Rios <brendamichelle995@gmail.com>
2020-06-30 11:16:52 -0400

    after lecture

--- 06-intro-to-sql-and-orms/orms/app/models/game.rb
+++ 06-intro-to-sql-and-orms/orms/app/models/game.rb
@@ -1,6 +1,6 @@
 class Game
   attr_accessor :id, :title, :genre, :price
-  @@all = []
+  # @@all = []
 
   def initialize(title, genre, price)
     @title = title
@@ -8,12 +8,26 @@ class Game
     @price = price
 
     # Task 2: replace with database
-    @@all << self
+    # @@all << self
+  end
+
+  def save
+    # get the ruby instance & insert it into the database
+    sql = "INSERT INTO games(title,genre,price) VALUES (?,?,?)"
+      # '#{self.title}','#{self.genre}','#{self.price}')"
+    DB[:conn].execute(sql, self.title, self.genre, self.price)
   end
 
 
   # Task 1: replace with database
   def self.all
-    @@all
+    # @@all
+    sql = "SELECT * FROM games;"
+    results = DB[:conn].execute(sql)
+
+    results.map do |row|
+      Game.new(row["title"],row["genre"],row["price"])
+    end
+    # binding.pry
   end
 end
