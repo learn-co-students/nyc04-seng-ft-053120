c9fc0ca84b0661a2698cde8728098d6d1982fd37
Michelle Rios <brendamichelle995@gmail.com>
2020-07-13 16:14:42 -0400

    after lecture

--- 14-sinatra-crud/nerdy-nummies/app/controllers/application_controller.rb
+++ 14-sinatra-crud/nerdy-nummies/app/controllers/application_controller.rb
@@ -18,7 +18,39 @@ class ApplicationController < Sinatra::Base
   # Task 1: Show a page with all the users
   get '/users' do
     @users = User.all
-    erb :users
+    erb :index
   end
+
+  # Task: display a form where a new user can be created
+  get '/users/new' do
+    erb :new
+  end
+
+  # Task: handle a post request & create the new user
+  # with the information from the form
+  post '/users' do
+    # "Hello World"
+    # binding.pry
+    user = User.create(params[:user])
+    redirect "/users/#{user.id}"
+  end
+
+
+  get '/users/:id' do
+    # "Hello World"
+    # binding.pry
+    @emoji = "😸"
+    @user = User.find(params[:id])
+    erb :show
+  end
+  
+  # get '/users/2' do
+  #   # "Hello World"
+  #   @user = User.find(2)
+  #   erb :show
+  # end
+
+
+
 
 end
