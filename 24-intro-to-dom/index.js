e789e5c4da4b70b4731f1683342de7d5f887c07b
Eric Kim <hyeokjungkim@gmail.com>
2020-08-03 13:57:50 -0400

    adds intro to dom

---
+++ 24-intro-to-dom/index.js
@@ -0,0 +1,15 @@
+let arr = [5,4,3,2,1]
+
+function isItEven(first_argument){
+    if(first_argument.length % 2 === 0){
+        console.log("IT IS EVEN")
+    } else {
+        console.log("IT IS ODD")
+    }
+}
+
+// Invoking a function with an argument
+isItEven(arr)
+
+// Referencing a function definition
+isItEven
